de46abe80b7058e995b110e55b39476bd043c972
Caike Bispo <caikbispo@hotmail.com>
2022-11-15 20:52:02 -0300

    FEAT(Main) Finalizando 1ª Aula

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,52 @@
 fun main(args: Array<String>) {
+    
+
+
+}
+
+private fun baseCollection() {
     println("Hello World!")
 
-    val nomes: Collection<String> = listOf("Caike", "Ingrid", "Jose", "Edinelva", "Leonardo", "Lucas")
+    var banco: bancoDeNomes = bancoDeNomes()
+    banco.adiciona("Caike")
+    banco.adiciona("Ingrid")
+    banco.adiciona("Pele")
+
+    var copia: Collection<String> = banco.nomes
+
+    banco.adiciona("Taylor")
+
+    println(copia)
+
+    println(banco.nomes)
+}
+
+class bancoDeNomes(){
+    val nomes: Collection<String> get() = Companion.dados.toList()
+    fun adiciona(nome: String){
+        Companion.dados.add(nome)
+    }
+    companion object {
+        private var dados = mutableListOf<String>()
+    }
+
+}
+
+
+private fun testaColecao() {
+    val nomes: Collection<String> = mutableListOf("Caike", "Ingrid", "Jose", "Edinelva", "Leonardo", "Lucas")
 
     nomes.forEach {
         println(it)
     }
 
+//    nomes.add("Souza")
+
     println("Tem Ingrid: ${nomes.contains("Ingrid")}")
 
     println("Size of: ${nomes.size}")
+//    nomes.remove("Souza")
+    println(nomes.plus("Marina"))
 
-    println(nomes.plus("Souza"))
-
-    println(nomes.random())
-
+    println("Nome aleatorio: ${nomes.random()}")
 }
