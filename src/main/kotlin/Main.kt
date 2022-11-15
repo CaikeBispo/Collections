0974b0d30d69447f30385b7864eab11aec76948b
Caike Bispo <caikbispo@hotmail.com>
2022-11-15 20:23:34 -0300

    FEAT(Main) General tests

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,18 @@
 fun main(args: Array<String>) {
     println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val nomes: Collection<String> = listOf("Caike", "Ingrid", "Jose", "Edinelva", "Leonardo", "Lucas")
+
+    nomes.forEach {
+        println(it)
+    }
+
+    println("Tem Ingrid: ${nomes.contains("Ingrid")}")
+
+    println("Size of: ${nomes.size}")
+
+    println(nomes.plus("Souza"))
+
+    println(nomes.random())
+
 }
