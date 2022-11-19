d962b273007cb0960e3c91dcea84fd5ea720abb2
Caike Bispo <caikbispo@hotmail.com>
2022-11-19 16:29:56 -0300

    FEAT (TestaMap) Testando comportamentos de escrita do Map

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,4 @@
 fun main(args: Array<String>) {
-    val pedidos = mapOf<Int, Double>(Pair(1, 10.99), Pair(2, 19.15), 3 to 7.15, 4 to 27.15)
-    //Infix é fortemente desaconselhado quando performance for exigida
+    TestaMap()
 
-    println(pedidos)
-
-    val pedido = pedidos[40]
-        pedido?.let{
-            println("Pedido $it")
-        }
-
-    for(pedido: Map.Entry<Int, Double> in pedidos){
-        println("Numero do pedido: ${pedido.key}")
-        println("Valor do pedido: ${pedido.value}")
-
-    }
 }
-
